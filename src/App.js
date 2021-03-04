4d99441729cbdb04e3daeaf06c9282156a9a2da9
shameeribnuilyas <48588765+shameeribnuilyas@users.noreply.github.com>
2021-03-04 21:27:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Donate from './components/Donate';
 import Privacy from './components/Privacy';
 import Terms from './components/Terms';
 import Policies from './components/Policies';
+import Container from './components/Container' ;
 
 import { Route, Switch } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App() {
       <TopBar/>
       <Switch>
         <Route exact path='/' component={Logo}/>
+        <Route exact path='/container' component={Container}/>
         <Route exact path='/about' component={About}/>
         <Route exact path='/donate' component={Donate}/>
         <Route exact path='/privacyPolicy' component={Privacy} />
